6ed6f9ebbde90c31d25f29b27ab893f2aaca7fda
Vince <babyaivan86@gmail.com>
2025-05-11 17:35:06 +0800

    Student Service Done.

--- src/main/java/com/project/codemic/Codemic/service/StudentService.java
+++ src/main/java/com/project/codemic/Codemic/service/StudentService.java
@@ -3,8 +3,16 @@ package com.project.codemic.Codemic.service;
 import com.project.codemic.Codemic.model.entity.Student;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentService {
     Student createStudent(Student student);
+
     List<Student> getAllStudents();
+
+    Optional<Student> getStudentById(Integer id);
+
+    Student updateStudent(Integer id, Student student);
+
+    void deleteStudent(Integer id);
 }
--- src/main/java/com/project/codemic/Codemic/service/StudentServiceImpl.java
+++ src/main/java/com/project/codemic/Codemic/service/StudentServiceImpl.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
     @Autowired
     private StudentRepository studentRepository;
+
 
     @Override
     public Student createStudent(Student student) {
@@ -21,5 +23,37 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
+    }
+
+    @Override
+    public Optional<Student> getStudentById(Integer id) {
+        return studentRepository.findById(id);
+    }
+
+    @Override
+    public Student updateStudent(Integer id, Student studentDetails) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+
+        if (studentOptional.isPresent()) {
+            Student updatedStudent = studentOptional.get();
+            updatedStudent.setFirstName(studentDetails.getFirstName());
+            updatedStudent.setMiddleName(studentDetails.getMiddleName());
+            updatedStudent.setLastName(studentDetails.getLastName());
+            updatedStudent.setEmail(studentDetails.getEmail());
+            updatedStudent.setPassword(studentDetails.getPassword());
+            updatedStudent.setProgram(studentDetails.getProgram());
+            updatedStudent.setSection(studentDetails.getSection());
+            updatedStudent.setYear(studentDetails.getYear());
+        }
+
+        return null;
+    }
+
+    @Override
+    public void deleteStudent(Integer id) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+
+        if (studentOptional.isPresent())
+            studentRepository.deleteById(id);
     }
 }
