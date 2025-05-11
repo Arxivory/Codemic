411462b7db30fc46d572083f0419d8eb0ca166b8
Vince <babyaivan86@gmail.com>
2025-05-11 18:39:07 +0800

    Finished the ActivityRO and created the InstructorRO

--- src/main/java/com/project/codemic/Codemic/model/request/ActivityRO.java
+++ src/main/java/com/project/codemic/Codemic/model/request/ActivityRO.java
@@ -1,4 +1,25 @@
 package com.project.codemic.Codemic.model.request;
 
-public record ActivityRO() {
+import com.project.codemic.Codemic.model.entity.Activity;
+import com.project.codemic.Codemic.model.entity.Subject;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ActivityRO(
+        Integer id,
+        @NotBlank(message = "Activity title is mandatory.") String title,
+        @NotBlank(message = "Activity content is mandatory.") String content,
+        @NotNull(message = "Subject ID id mandatory") Integer subjectId
+) {
+    public Activity toEntity(Activity activity, Subject subject) {
+        if (activity == null) {
+            activity = new Activity();
+        }
+
+        activity.setTitle(title);
+        activity.setContent(content);
+        activity.setSubject(subject);
+
+        return activity;
+    }
 }
---
+++ src/main/java/com/project/codemic/Codemic/model/request/InstructorRO.java
@@ -0,0 +1,43 @@
+package com.project.codemic.Codemic.model.request;
+
+import com.project.codemic.Codemic.model.entity.Instructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record InstructorRO(
+        Integer id,
+
+        @NotBlank(message = "First name is mandatory.")
+        String firstName,
+
+        @NotBlank(message = "Last name is mandatory.")
+        String lastName,
+
+        @NotNull(message = "Middle name initial is mandatory.")
+        Character middleName,
+
+        @NotBlank(message = "Email is mandatory.")
+        @Email(message = "Email should be valid.")
+        String email,
+
+        @NotBlank(message = "Password is mandatory.")
+        @Size(min = 8, message = "Password must be at least 8 characters long.")
+        String password
+) {
+
+    public Instructor toEntity(Instructor instructor) {
+        if (instructor == null) {
+            instructor = new Instructor();
+        }
+
+        instructor.setFirstName(firstName);
+        instructor.setMiddleName(middleName);
+        instructor.setLastName(lastName);
+        instructor.setEmail(email);
+        instructor.setEmail(password);
+
+        return instructor;
+    }
+}
