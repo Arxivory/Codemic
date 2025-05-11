7c2a4252c222cfe0eab9c382dd38d5f7802ca5bd
shaiprdr <159556480+shaiprdr@users.noreply.github.com>
2025-05-11 15:52:43 +0800

    Update Page.java

--- src/main/java/com/project/codemic/Codemic/model/entity/Page.java
+++ src/main/java/com/project/codemic/Codemic/model/entity/Page.java
@@ -1,1 +1,32 @@
+package com.project.codemic.Codemic.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "pages")
+public class Page implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 8397608760754233645L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "module_id", nullable = false)
+    private Module module;
+
+}
