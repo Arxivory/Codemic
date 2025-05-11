db4e0771e06ff99fa1d0b4ebf8319c50325fd17f
shaiprdr <159556480+shaiprdr@users.noreply.github.com>
2025-05-11 11:14:24 +0800

    Update SubjectService.java

--- src/main/java/com/project/codemic/Codemic/service/SubjectService.java
+++ src/main/java/com/project/codemic/Codemic/service/SubjectService.java
@@ -1,1 +1,19 @@
+package com.project.codemic.Codemic.service;
 
+import com.project.codemic.Codemic.model.entity.Subject;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SubjectService {
+    
+    Subject createSubject(Subject subject);
+    
+    List<Subject> getAllSubjects();
+    
+    Optional<Subject> getSubjectById(Integer id);
+    
+    Subject updateSubject(Integer id, Subject subjectDetails);
+    
+    void deleteSubject(Integer id);
+}
