c9263def910ab30e56a75cf17624a8ba679b676a
shaiprdr <159556480+shaiprdr@users.noreply.github.com>
2025-05-11 15:54:51 +0800

    Update ModuleRO.java

--- src/main/java/com/project/codemic/Codemic/model/request/ModuleRO.java
+++ src/main/java/com/project/codemic/Codemic/model/request/ModuleRO.java
@@ -1,1 +1,27 @@
+package com.project.codemic.Codemic.model.request;
 
+import java.util.Objects;
+
+import com.project.codemic.Codemic.model.entity.Module;
+import com.project.codemic.Codemic.model.entity.Subject;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ModuleRO(
+    Integer id,
+    @NotBlank(message = "Title is mandatory.") String title,
+    @NotNull(message = "Subject ID is mandatory.") Integer subjectId
+) {
+
+    public Module toEntity(Module module, Subject subject) {
+        if (Objects.isNull(module)) {
+            module = new Module();
+        }
+        
+        module.setTitle(title);
+        module.setSubject(subject);
+        
+        return module;
+    }
+}
