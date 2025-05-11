e275c7c066e32f03a07473b9786d5e55f0028bd4
shaiprdr <159556480+shaiprdr@users.noreply.github.com>
2025-05-11 15:49:41 +0800

    Update PageRO.java

--- src/main/java/com/project/codemic/Codemic/model/request/PageRO.java
+++ src/main/java/com/project/codemic/Codemic/model/request/PageRO.java
@@ -1,41 +1,26 @@
 package com.project.codemic.Codemic.model.request;
 
-import java.util.Date;
 import java.util.Objects;
 
+import com.project.codemic.Codemic.model.entity.Module;
 import com.project.codemic.Codemic.model.entity.Page;
-import com.project.codemic.Codemic.model.entity.Subject;
-import com.project.codemic.Codemic.util.DateUtils;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record PageRO(
-    Long id,
-    @NotBlank(message = "Title is mandatory.") 
-    @Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters skrt eme.")
-    String title,
-    
-    @NotBlank(message = "Content is mandatory.")
-    String content,
-    
-    Date createdDate,
-    
-    @NotNull(message = "Subject ID is mandatory.") 
-    Integer subjectId
+    Integer id,
+    @NotBlank(message = "Content is mandatory.") String content,
+    @NotNull(message = "Module ID is mandatory.") Integer moduleId
 ) {
 
-    public Page toEntity(Page page, Subject subject) {
+    public Page toEntity(Page page, Module module) {
         if (Objects.isNull(page)) {
             page = new Page();
         }
         
-        page.setTitle(title);
         page.setContent(content);
-        page.setCreatedDate(Objects.isNull(createdDate) ? DateUtils.now() : createdDate);
-        page.setSubject(subject);
-
+        page.setModule(module);
         return page;
     }
 }
