2b34b93357381ee58a30c449ff0e83531592a482
shaiprdr <159556480+shaiprdr@users.noreply.github.com>
2025-05-11 11:20:17 +0800

    Update SubjectRepository.java

--- src/main/java/com/project/codemic/Codemic/repository/SubjectRepository.java
+++ src/main/java/com/project/codemic/Codemic/repository/SubjectRepository.java
@@ -4,6 +4,17 @@ import com.project.codemic.Codemic.model.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SubjectRepository extends JpaRepository <Subject, Integer> {
+  
+    Optional<Subject> findByName(String name);
+    
+    List<Subject> findByCreditsGreaterThanEqual(Integer credits);
+    
+    List<Subject> findByNameContainingIgnoreCase(String keyword);
+    
+    boolean existsByName(String name);
 }
