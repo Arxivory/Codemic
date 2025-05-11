b35407b98077008e547ff9a8f51052e644f0b913
shaiprdr <159556480+shaiprdr@users.noreply.github.com>
2025-05-11 16:26:22 +0800

    Update StudentSubjectRO.java

--- src/main/java/com/project/codemic/Codemic/model/request/StudentSubjectRO.java
+++ src/main/java/com/project/codemic/Codemic/model/request/StudentSubjectRO.java
@@ -1,1 +1,31 @@
+package com.project.codemic.Codemic.model.request;
 
+import java.util.Objects;
+
+import com.project.codemic.Codemic.model.entity.Student;
+import com.project.codemic.Codemic.model.entity.StudentSubject;
+import com.project.codemic.Codemic.model.entity.Subject;
+
+import jakarta.validation.constraints.NotNull;
+
+public record StudentSubjectRO(
+    Integer id,
+    
+    @NotNull(message = "Student ID is mandatory.")
+    Integer studentId,
+    
+    @NotNull(message = "Subject ID is mandatory.")
+    Integer subjectId
+) {
+
+    public StudentSubject toEntity(StudentSubject studentSubject, Student student, Subject subject) {
+        if (Objects.isNull(studentSubject)) {
+            studentSubject = new StudentSubject();
+        }
+        
+        studentSubject.setStudent(student);
+        studentSubject.setSubject(subject);
+        
+        return studentSubject;
+    }
+}
